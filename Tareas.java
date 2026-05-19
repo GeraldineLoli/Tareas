07976b4d4aedd67c17a011cec2a13cfcf77efd07
Geraldine Eloisa Loli Espejo <gery.loli0105@gmail.com>
2026-05-19 16:12:18 -0500

    Primer commit

---
+++ Tareas.java
@@ -0,0 +1,7 @@
+public class Tareas {
+    public static void main(String[] args) {
+        System.out.println("Tarea 1: Crear repositorio");
+        System.out.println("Tarea 2: Configurar ramas");
+        // Agregar más tareas aquí
+    }
+}
